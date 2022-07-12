47770d66798f69c71e0bb422d74fd0dc6c49e50d
surec <103966120+surec-or@users.noreply.github.com>
2022-07-11 19:34:18 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var input = document.getElementById("terminalEntry");
+var cmd;
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    cmd = document.getElementById("terminalEntry").value;
+    console.log(cmd)
+    event.preventDefault();
+    document.getElementById("enterButton").click();
+  if (cmd.toUpperCase() === "HELP"){
+    document.getElementById("commandList").style.color="lime";
+  }else{
+    document.getElementById("commandList").style.color="black";
+    if (cmd.toUpperCase() === "PROJECTS"){
+      window.location = "subpages/projects/projects.html"
+    }else if (cmd.toUpperCase() === "LINKS"){
+      window.location = "subpages/links/links.html"
+  }
+  }
+}});
